95921dcd70f130e53cced71871b74bc13aee16d4
chancka <chancka@Ubuntu.myguest.virtualbox.org>
2023-01-25 16:37:32 +0100

    Add script python cookies

---
+++ www/cookies/cookies.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+import os, time
+
+VERSION = 0.1
+
+def get_cookie(_match): 
+  # Returns the value from the matching cookie or '' if not defined.
+  if 'HTTP_COOKIE' in os.environ:
+    cookies = os.environ['HTTP_COOKIE']
+    cookies = cookies.split('; ')
+
+    for cookie in cookies:
+      (_name, _value) = cookie.split('=')
+      if (_match.lower() == _name.lower()):
+        return _value
+  return('')
+
+def val(_string): 
+  # Returns the integer value of a string or zero if not numeric.
+  try:
+    return int(_string)
+  except:
+    return 0
+
+print ('Content-Type: text/html;charset=utf-8' )
+if not get_cookie('first'): # Don't redefine value if aleady defined.
+  print ('Set-Cookie: First=' + time.asctime(time.localtime()))
+print ('Set-Cookie: Last=' + time.asctime(time.localtime()))
+print ('Set-Cookie: Count=' + str(val(get_cookie('Count')) + 1)) # Increment counter.
+print ()# End of header.
+
+print ('<html><body><table border="0">')
+if get_cookie('first'): # Only dispay value if cookie is defined.
+  print ('<tr><td>Time First Visit</td><td>:</td><td>' + get_cookie('first') + '</tr>')
+if get_cookie('last'): # Only dispay value if cookie is defined.
+  print ('<tr><td>Time Last Visit</td><td>:</td><td>' + get_cookie('last') + '</tr>')
+print ('<tr><td>Counter</td><td>:</td><td>' + str(val(get_cookie('count'))) + '</tr>')
+print ('</table></body></html>')
