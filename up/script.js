f5e2d7df55bd21c8de042477357287329f36cca4
Maxime ARNAUDY <maxime.arnaudy@gmail.com>
2023-01-26 13:39:50 +0100

    Add delete file from browser via CGI

--- up/script.js
+++ up/script.js
@@ -8,3 +8,12 @@ function uploadFile() {
 	  console.error("Error:", error);
 	});
 }
+
+function deleteFile() {
+	const fileName = document.getElementById("file-input-del").value;
+	fetch("delete.php/upload/" + fileName, {
+	  method: "DELETE"
+	}).catch(error => {
+	  console.error("Error:", error);
+	});
+}
